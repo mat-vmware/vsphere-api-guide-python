090c117d0b8cb4f2357451fe1b279501916d2cfc
mat-vmware <mat@vmware.com>
2015-04-14 12:09:10 +0800

    Adding clone vm.

---
+++ test1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#
+# VMware vSphere Python SDK
+# Copyright (c) 2008-2015 VMware, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Python program for powering on vms on a host on which hostd is running
+"""
+
+from __future__ import print_function
+
+from pyVim.connect import SmartConnect, Disconnect
+from pyVmomi import vim, vmodl
+
+import argparse
+import atexit
+import getpass
+import sys
+
+def getArgs(*args, **kwargs):
+   print(args)
+   print(*args)
+   print(kwargs)
+   print(*kwargs)
+   print(**kwargs)
+
+def main():
+   """
+   Test1 
+   """
+   print("hello")
+   getArgs(1, 2)
+   getArgs(1, 2, {"key1": "word", "name1": "value"})
+   getArgs(1, 2, key1="word", name1="value")
+
+# Start program
+if __name__ == "__main__":
+   main()
